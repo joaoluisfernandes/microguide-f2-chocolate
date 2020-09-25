c16fab72a92540b094f8b6beda5ae37fa0794f04
joaoluisfernandes <71817909+joaoluisfernandes@users.noreply.github.com>
2020-09-25 11:43:15 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Shop 1")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Shop 3")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Shop 2")
+})
+basic.showString("Welcome to MicroGuide!")
+basic.forever(function () {
+	
+})
